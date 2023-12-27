2efb275acbaed79e23a45b0c45b0a8d3bd48979f
Rian Lopes <77791275+rianbl@users.noreply.github.com>
2023-12-26 22:13:27 -0300

    Update dagSpark.py

--- dags/dagSpark.py
+++ dags/dagSpark.py
@@ -1,11 +1,11 @@
 from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
+from airflow.providers.cncf.operators.kubernetes_pod import KubernetesPodOperator
 
 # Define default_args and DAG configuration
 default_args = {
     'owner': 'your_username',
-    'start_date': datetime(2023, 1, 1),
+    'start_date': datetime(2022, 1, 1),  # Set to a past date
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
@@ -15,12 +15,16 @@ dag = DAG(
     default_args=default_args,
     description='DAG to trigger Spark job',
     schedule_interval='*/15 * * * *',  # Run every 15 minutes
+    catchup=False,  # Do not backfill for past intervals
 )
 
-# Define Spark job task
-spark_job_task = BashOperator(
+# Define Spark job task using KubernetesPodOperator
+spark_job_task = KubernetesPodOperator(
     task_id='run_spark_job',
-    bash_command='kubectl exec -it spark-master-0 -- /opt/bitnami/spark/bin/spark-submit --master spark://spark-master-svc:7077 --name helloWorld /opt/bitnami/spark/apps/HelloWorld.py',  # Adjust as needed
+    name='spark-job-task',
+    namespace='your_namespace',  # Set your Kubernetes namespace
+    image='your_spark_image',  # Set the Spark image
+    cmds=['/opt/bitnami/spark/bin/spark-submit', '--master', 'spark://spark-master-svc:7077', '--name', 'helloWorld', '/opt/bitnami/spark/apps/HelloWorld.py'],
     dag=dag,
 )
 
