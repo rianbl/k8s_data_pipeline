0cae09df66cfa054b1a11e199bbdd5e974060236
Rian Lopes <77791275+rianbl@users.noreply.github.com>
2024-01-08 20:12:07 -0300

    Update dagSpark.py

--- dags/dagSpark.py
+++ dags/dagSpark.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from airflow.operators.bash_operator import BashOperator
 import os
 
@@ -21,20 +22,9 @@ dag = DAG(
 )
 
 # Print the current working directory
-spark_job_task = SparkPodOperator(
-    task_id='run_spark_job',
-    name='spark-job-task',
-    namespace='default',  # Set your Kubernetes namespace
-    image='bitnami/spark:3.5.0-debian-11-r16',  # Set the Spark image
-    spark_pod='spark-master-0',  # Specify the existing Spark pod
-    cmds=[
-        '/opt/bitnami/spark/bin/spark-submit',
-        '--conf', 'spark.jars.ivy=/tmp/.ivy',  # Set Ivy directory
-        '--master', 'spark://spark-master-svc:7077',
-        '--name', 'helloWorld',
-        '/opt/bitnami/spark/apps/HelloWorld.py'
-    ],
-    dag=dag,
+submit_job = SparkSubmitOperator(
+    application="${SPARK_HOME}/examples/src/main/python/pi.py",
+    task_id="submit_job"
 )
 
 # Define Spark job task using KubernetesPodOperator
