063194e165b31ebc720db11a2d454043b6eccbfc
Rian Lopes <77791275+rianbl@users.noreply.github.com>
2024-01-09 00:35:18 -0300

    Update dagSpark.py

--- dags/dagSpark.py
+++ dags/dagSpark.py
@@ -23,7 +23,7 @@ dag = DAG(
 spark_task = SparkKubernetesOperator(
     task_id='run_spark_job',
     namespace='default',
-    application_file='/path/to/your/spark_application.yaml',  # or pass a dictionary
+    application_file='/mnt/host/apps/HelloWorld.py',  # or pass a dictionary
     kubernetes_conn_id='my_k8s_id',  # specify your Kubernetes connection ID
     dag=dag,
 )
