015c510b67e84c3e2651fd7fe9045d294522d393
Rian Lopes <77791275+rianbl@users.noreply.github.com>
2024-01-09 16:14:19 -0300

    Update dagSpark.py

--- dags/dagSpark.py
+++ dags/dagSpark.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from airflow import DAG
+from kubernetes.client import V1Volume, V1VolumeMount
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
 import os
